e5148c87e2eca3474ea814d0b59e29c04a03c928
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 13:52:29 +0100

    app.js file

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ussdRouter = require("./index");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.listen(PORT, () => console.log(`listening to ${PORT}`));
