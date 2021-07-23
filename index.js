c78fec3d8a26eff29ee30b5bbdad48001dcdb26b
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 15:10:23 +0100

    app.js file

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+
+// Get authentication
+// const credentials = {
+//     apiKey: process.env.APIKEY, // use your sandbox app API key for development in the test environment
+//     username: process.env.USER_NAME // use 'sandbox' for development in the test environment
+//   };
+//   console.log(credentials);
+//   const AfricasTalking = require("africastalking")(credentials);
+
 router.post("/", (res, req) => {
     const { sessionId, serviceCode, phoneNumber, text} = req.body;
 
