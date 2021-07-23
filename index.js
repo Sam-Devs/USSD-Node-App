1c9cf7e14cb2527105f28ed60f3a0a24f2571f87
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-23 17:24:22 +0100

    ussd info

--- index.js
+++ index.js
@@ -18,15 +18,29 @@ router.post("/", (req, res) => {
 
     let response = "";
 
-    if(text == "") {
-        response = `What would you like to check?
-        1. My Account
-        2. My Balance
-        `
-    } else if(text == "1") {
-        
+    if (text == '') {
+        // This is the first request. Note how we start the response with CON
+        response = `What would you like to check
+        1. My account
+        2. My phone number`;
+    } else if ( text == '1') {
+        // Business logic for first level response
+        response = `Choose account information you want to view
+        1. Account number`;
+    } else if ( text == '2') {
+        // Business logic for first level response
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your phone number is ${phoneNumber}`;
+    } else if ( text == '1*1') {
+        // This is a second level response where the user selected 1 in the first instance
+        const accountNumber = 'ACC100101';
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your account number is ${accountNumber}`;
     }
-    res.send("END yes .....")
+
+    // Send the response back to the API
+    res.set('Content-Type: text/plain');
+    res.send(response);
 });
 
 module.exports = router;
